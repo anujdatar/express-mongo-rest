9879fc570d43e0a58afe46dc0a6250e100e9c8ff
Anuj Datar <anuj.datar@gmail.com>
2019-08-22 05:44:06 -0400

    basic get and post, save to mongo

--- server.js
+++ server.js
@@ -1,57 +1,29 @@
-import express from 'express'
-import path from 'path'
-import bodyParser from 'body-parser'
-import mongojs from 'mongojs'
-import os from 'os'
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv/config')
 
-// init express app/server
-let app = express()
+const server = express()
 
-// start server and log server address and port
-app.listen(3000, () => {
-  const networkInterfaces = os.networkInterfaces()
-  const networkIp = networkInterfaces['Wi-Fi'][1]['address']
-  const localIp = networkInterfaces['Loopback Pseudo-Interface 1'][1]['address']
-  console.log('Express.js server running at')
-  console.log('- Local:   http://'+localIp +':3000/  or  http://localhost:3000/')
-  console.log('- Network: http://'+networkIp +':3000/')
-  // console.log(os.networkInterfaces())
+const postsRoute = require('./routes/posts')
+
+// middleware for body-parser
+server.use(bodyParser.json())
+
+// middleware for routes
+server.use('/posts', postsRoute)
+
+// Routes
+server.get('/', (req, res) => {
+  res.send('Home page')
 })
 
-// Body Parser middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  () => { console.log('Connected to DB') })
 
-// enable CORS on server
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
-
-// connect to mongodb database
-const db = mongojs('MealPlanner')
-// let ObjectId = mongojs.ObjectId
-
-// render home page just in case
-app.get('/', (req, res) => {
-  db.menuList.find( (err, docs) => {
-    res.send(docs)
-  })
-})
-
-// read database documents and send to web page
-app.get('/getListOfItems', (req, res) => {
-  db.menuList.find( (err, docs) => {
-    if (err) {
-      throw err
-    }
-    res.send(docs)
-  })
-})
-
-app.post('/addItemToDb', (req, res) => {
-  console.log(req.body)
-  // console.log(req.body.selected)
-  res.send('success')
+// start server
+server.listen(3000, () => {
+  console.log('Express server running at http://localhost:3000')
 })
