4a755e1ddbb3d20112e70cdeaabc114e94f95907
Anuj Datar <anuj.datar@gmail.com>
2023-08-29 12:11:21 +0530

    clear authToken on logout

--- src/functions/user/logout.ts
+++ src/functions/user/logout.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from 'express'
 
 export const logout = (_req: Request, res: Response): void => {
+  res.clearCookie('authToken')
   res.status(200)
   res.send({
     message: 'User logged out successfully'
